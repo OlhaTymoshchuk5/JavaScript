3e231ed4e215fcd2e26d386e38994d323bdf6e06
OlhaTymoshchuk5 <58119179+OlhaTymoshchuk5@users.noreply.github.com>
2019-11-23 18:39:21 -0500

    Update click.js

--- js/click.js
+++ js/click.js
@@ -1,12 +1,10 @@
 //function clikc the enter
 function clikcEnter(ref){
     if (window.event.keyCode === 13){
-        //get value from the text box
        var tbValue = document.getElementById(ref.id).value;
         document.getElementById(`${GLOBALROW}_${GLOBALCOLLUMN}`).innerHTML = tbValue;
         //put the index into array
         tblArray[GLOBALROW - 1][GLOBALCOLLUMN - 1] = tbValue;
-        //we have to do -1 because the index starts with 0
         calculateCell(GLOBALROW - 1, GLOBALCOLLUMN - 1);
         recalculate();
     }
@@ -14,9 +12,7 @@ function clikcEnter(ref){
 function clickCell(clickCell) {
     //get the cell id
     let cellId = clickCell.id;
-    //output the id to the console
     console.log(cellId);
-    //shows the index of the cell with the '_'
     var indexOfCell = clickCell.id.split('_');
     indexOfCell[0] = parseInt(indexOfCell[0]);
     indexOfCell[1] = parseInt(indexOfCell[1]);
